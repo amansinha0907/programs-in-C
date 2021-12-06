99c8071e630c61b3c3940051d3cb6e46427ab21a
Akash Pandey <73417064+akashpandey28@users.noreply.github.com>
2021-12-06 23:33:33 +0530

    Create printhello.c
    Basic input output program

---
+++ printhello.c
@@ -0,0 +1,7 @@
+//In this program you will learn to display any message on screen.
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
