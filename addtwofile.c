2c85683c830b31a87b1bd09ba92277d32a4fb5fc
Akash Pandey <73417064+akashpandey28@users.noreply.github.com>
2021-12-06 23:37:36 +0530

    Create addtwofile.c
    Addition of two file

---
+++ addtwofile.c
@@ -0,0 +1,15 @@
+// In this program you will learn to print the addition of two number.
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
