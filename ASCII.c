0ca15fb12d2fd6c06b73842586ae10dbd8eebc25
amansinha0907 <96279990+amansinha0907@users.noreply.github.com>
2021-12-17 11:23:53 +0530

    bhai bhai

--- ASCII.c
+++ ASCII.c
@@ -4,7 +4,7 @@ int main() {
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);  
-    
+    //amansinha
     // %d displays the integer value of a character
     // %c displays the actual character
     printf("ASCII value of %c = %d", c, c);
